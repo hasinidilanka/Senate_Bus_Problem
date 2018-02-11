d18c9af71a98fb9f6b309dcb79a853fb65f9afe3
hasinidilanka <hasinidilanka@gmail.com>
2018-02-11 15:14:22 +0530

    Initializing varables.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.util.concurrent.Semaphore;
+
 public class Main {
 
+    //Initializing variables
+
+
+    public static  int riders = 0;
+    public static Semaphore mutex = new Semaphore(1);
+    public static Semaphore multiplex = new Semaphore(50);
+    public static Semaphore bus = new Semaphore(0);
+    public static Semaphore allAboard = new Semaphore(0);
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println("Senate Bus Problem");
     }
 }
